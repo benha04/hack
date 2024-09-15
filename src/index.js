6b8b6095d1a2ea0a7cf32c7510b0692bc4cd7747
Patrick Badiang <patrickvyn@gmail.com>
2024-09-15 04:03:53 -0400

    shortened index

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 
 
-
-
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
