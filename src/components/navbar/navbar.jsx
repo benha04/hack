36bd69d5cba14f617940bed04e33863cc8411e8f
Patrick Badiang <patrickvyn@gmail.com>
2024-09-15 10:10:02 -0400

    Changed Avatar path to /images/logo.png

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
         alignItems: 'center',
       }}
     >
-      <Avatar alt="Remy Sharp" src="images/logo.png" sx={{ width: 128, height: 128 }} />
+      <Avatar alt="Remy Sharp" src="/images/logo.png" sx={{ width: 128, height: 128 }} />
       <Typography variant= "h1" style={{ marginLeft: '16px' }} color="white">Gobbler Gauntlet</Typography> {/* You can customize the logo or title here */}
     </Box>
       <div className="navbar-right">
